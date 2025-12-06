945102d06b9c9194724d876ac23279b9a15a568f
ShadowForgelab <waigaonkaralok@gmail.com>
2025-12-06 14:51:32 +0530

    Grid Pattern Printing

---
+++ PatternPrinting/Grid_Patterns/GTS1.java
@@ -0,0 +1,17 @@
+
+public class GTS1 {
+    public static void main(String[] args) {
+        int n = 7 ; 
+
+        for(int row =1 ; row <=n ; row ++){
+            for(int col = 1; col <= n ; col++){
+                if(row == col || row + col == n+1){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
