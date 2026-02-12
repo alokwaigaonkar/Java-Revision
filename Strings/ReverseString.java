eb2074114799656b374f4562c4033f002eb6b1d2
ShadowForgelab <waigaonkaralok@gmail.com>
2026-02-12 18:04:35 +0530

    Reverse a String

---
+++ Strings/ReverseString.java
@@ -0,0 +1,27 @@
+public class ReverseString {
+
+    public static String reverseString(String str){
+        if(str == null || str.isEmpty()){
+            return "Invalid Input";
+        }
+        int n = str.length();
+        int left = 0;
+        int right = n-1;
+
+        char [] collector = str.toCharArray();
+
+        while(left < right){
+            char temp = collector[left];
+            collector[left] = collector[right];
+            collector[right] = temp;
+
+            left++;
+            right--;
+
+        }
+        return new String(collector);
+    }
+    public static void main(String args[]){
+        System.out.println(reverseString("programming"));
+    }
+}
